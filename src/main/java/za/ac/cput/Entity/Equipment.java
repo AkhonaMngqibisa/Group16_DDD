dbc501c6acabd58e0420078aad3f1e6f54e1a5b2
Lilitha Moni <lilithamoni@gmail.com>
2021-06-10 17:59:41 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/Entity/Equipment.java
@@ -0,0 +1,66 @@
+package za.ac.cput.Entity;
+
+public class Equipment {
+    private int eqId;
+    private String eqName, eqModel, eqInfo;
+
+    private Equipment(Builder builder) {
+        this.eqId = builder.eqId;
+        this.eqName = builder.eqName;
+        this.eqModel = builder.eqModel;
+        this.eqInfo = builder.eqInfo;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "Equipment Id ='" + eqId + '\'' +
+                ", Name ='" + eqName + '\'' +
+                ", Model ='" + eqModel + '\'' +
+                ", Info ='" + eqInfo + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int eqId;
+        private String eqName, eqModel, eqInfo;
+
+        public Builder setEqId(int eqId) {
+            this.eqId = eqId;
+            return this;
+        }
+
+        public Builder setEqName(String eqName) {
+            this.eqName = eqName;
+            return this;
+        }
+
+        public Builder setEqModel (String eqModel) {
+            this.eqModel = eqModel;
+            return this;
+        }
+
+        public Builder setEqInfo (String eqInfo) {
+            this.eqInfo = eqInfo;
+            return this;
+        }
+
+
+
+        public Builder copy(Equipment equipment) {
+            this.eqId = equipment.eqId;
+            this.eqName = equipment.eqName;
+            this.eqModel = equipment.eqModel;
+            this.eqInfo = equipment.eqInfo;
+            return this;
+        }
+
+        public Equipment build() {
+
+            return new Equipment(this);
+        }
+    }
+}
+
---
+++ src/main/java/za/ac/cput/Entity/Supplier.java
@@ -0,0 +1,57 @@
+package za.ac.cput.Entity;
+
+public class Supplier {
+    private int supId, eqId;
+    private String supName;
+
+    private Supplier(Builder builder) {
+        this.supId = builder.supId;
+        this.supName = builder.supName;
+        this.eqId = builder.eqId;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "Supplier Id ='" + supId + '\'' +
+                ", Name ='" + supName + '\'' +
+                ", Equipment Id ='" + eqId + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int supId,eqId;
+        private String supName;
+
+        public Builder setSupId(int supId) {
+            this.supId = supId;
+            return this;
+        }
+
+        public Builder setSupName(String supName) {
+            this.supName = supName;
+            return this;
+        }
+
+        public Builder setEqId(int eqId) {
+            this.eqId = eqId;
+            return this;
+        }
+
+
+
+        public Builder copy(Supplier supplier) {
+            this.supId = supplier.supId;
+            this.supName = supplier.supName;
+            this.eqId = supplier.eqId;
+            return this;
+        }
+
+        public Supplier build() {
+
+            return new Supplier(this);
+        }
+    }
+}
