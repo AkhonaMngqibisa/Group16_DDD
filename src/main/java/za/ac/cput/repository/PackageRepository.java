264a5f9ee81a3bece2d274599a089d17dc59635f
Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>
2021-07-26 01:42:26 +0200

    Added Repository

---
+++ src/main/java/za/ac/cput/repository/PackageRepository.java
@@ -0,0 +1,66 @@
+package za.ac.cput.repository;
+/**
+ PackageRepository.java
+ Repository: PackageRepository
+ Author: Zimkhitha Nkqenkqa 216280427
+ Date: 25 July 2021
+ */
+
+import za.ac.cput.entity.Package;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PackageRepository implements IPackageRepository {
+    private static PackageRepository packageRepository = null;
+    private Set<Package> packageSet;
+
+    private PackageRepository() {
+        this.packageSet = new HashSet<>();
+    }
+
+    public static IPackageRepository getPackageRepository() {
+        if (packageRepository == null) {
+            packageRepository = new PackageRepository();
+        }
+        return new PackageRepository();
+    }
+
+    @Override
+    public Package create(Package aPackage) {
+        this.packageSet.add(aPackage);
+        return aPackage;
+    }
+
+    @Override
+    public Package read(Integer integer) {
+        for (Package aPackage : this.packageSet) {
+            return aPackage;
+        }
+        return null;
+    }
+
+    @Override
+    public Package update(Package aPackage) {
+        Package update = read(aPackage.getPackageID());
+        if (update != null)
+            this.packageSet.remove(update);
+            this.packageSet.add(aPackage);
+        return aPackage;
+    }
+
+    @Override
+    public boolean delete(Integer integer) {
+        Package aPackage = read(integer);
+        if (aPackage != null) {
+            this.packageSet.remove(aPackage);
+            return true;
+        } else {
+            return false;
+        }
+    }
+        @Override
+        public Set<Package> getAll () {
+            return this.packageSet;
+        }
+    }
---
+++ src/test/java/za/ac/cput/repository/ReportRepositoryTest.java
@@ -0,0 +1,38 @@
+package za.ac.cput.repository;
+/**
+ ReportRepositoryTest.java
+ Repository: ReportRepositoryTest
+ Author: Zimkhitha Nkqenkqa 216280427
+ Date: 25 July 2021
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportRepositoryTest {
+
+    @Test
+    void geReportRepository() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
