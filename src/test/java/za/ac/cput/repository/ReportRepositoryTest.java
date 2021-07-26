c533113d4d9b0217259dc6eb59d9ea7072d57232
Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>
2021-07-26 14:39:29 +0200

    Added Repository Tests

--- src/test/java/za/ac/cput/repository/ReportRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/ReportRepositoryTest.java
@@ -7,32 +7,54 @@ package za.ac.cput.repository;
  */
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Report;
+import za.ac.cput.factory.ReportFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ReportRepositoryTest {
+public class ReportRepositoryTest {
+    private static IReportRepository reportRepository = ReportRepository.geReportRepository();
+    private static Report report = ReportFactory.createReport(022,
+            0021,
+            "2021/03/16",
+            "Weekly");
 
     @Test
-    void geReportRepository() {
+    public void geReportRepository() {
     }
 
     @Test
-    void create() {
+    public void create() {
+        Report report1 = reportRepository.create(reportRepository.create(report));
+        assertEquals(report.getReportID(), report.getReportID());
+        System.out.println("\nCreated: " + report);
     }
 
     @Test
-    void read() {
+    public void read() {
+        Report read = reportRepository.read(report.getReportID());
+        assertNotNull(read);
+        System.out.println("\nTo String:\n" + read.toString());
     }
 
     @Test
-    void update() {
+    public void update() {
+        Report report1 = new Report.Builder().copy(report).setReportID(045).
+                setTrainerID(0015).setReportDate
+                ("2021/08/26").setAttendance
+                ("Monthly").build();
+        report1 = reportRepository.update(report1);
+        System.out.println("\nUpdated: " + report1);
     }
 
     @Test
-    void delete() {
+    public void delete() {
+        boolean deleteSuccessful = reportRepository.delete(report.getReportID());
+        assertTrue(deleteSuccessful);
     }
 
     @Test
-    void getAll() {
+    public void getAll() {
+        System.out.println("\nDisplay All: " + reportRepository.getAll());
     }
 }
