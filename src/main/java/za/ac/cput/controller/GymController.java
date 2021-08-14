0c53a984400ec095da81a7ac00fddb7ad0816bf9
AkhonaMngqibisa <bakhonamngqibisa@gmail.com>
2021-08-14 02:12:34 +0200

    Added Controller

---
+++ src/main/java/za/ac/cput/controller/GymController.java
@@ -0,0 +1,11 @@
+package za.ac.cput.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gym")
+public class GymController {
+
+
+}
---
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -0,0 +1,14 @@
+package za.ac.cput.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController
+{
+    @RequestMapping({"/", "/home"})
+    String home() {
+        return "Welcome to GymManagement Application!";
+    }
+}
