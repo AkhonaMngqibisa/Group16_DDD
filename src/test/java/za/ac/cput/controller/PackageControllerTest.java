c22d17778cfa288788f1820c80dae18e4028af30
Zimmy Nkqenkqa <zimkhitha.nkqenkqa@gmail.com>
2021-08-16 23:04:14 +0200

    Controller Tests

--- src/test/java/za/ac/cput/controller/PackageControllerTest.java
+++ src/test/java/za/ac/cput/controller/PackageControllerTest.java
@@ -3,31 +3,83 @@ package za.ac.cput.controller;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import za.ac.cput.entity.Package;
+import za.ac.cput.factory.PackageFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class PackageControllerTest {
+    private static Package aPackage = PackageFactory.createPackage(015, 025,
+            8, 350.00,
+            "Mega Package");
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private static final String baseURL = "http://localhost:8080/package";
 
     @Test
     void create() {
+        String url = baseURL+"/create";
+        System.out.println(url);
+
+        ResponseEntity<Package> postResponseEntity = restTemplate.postForEntity(url, aPackage,Package.class);
+        assertNotNull(postResponseEntity);
+        assertNotNull(postResponseEntity.getBody());
+        assertEquals(postResponseEntity.getStatusCode(), HttpStatus.OK);
+        aPackage = postResponseEntity.getBody();
+
+        System.out.println(aPackage);
+        assertEquals(aPackage.getPackageID(),postResponseEntity.getBody().getPackageID());
     }
 
     @Test
     void read() {
+        String url = baseURL + "read/" + aPackage.getPackageID();
+        System.out.println(url);
+
+        ResponseEntity<Package> readResponseEntity = restTemplate.getForEntity(url, Package.class);
+        assertEquals(aPackage.getPackageID(), readResponseEntity.getBody().getPackageID());
+
+        System.out.println(readResponseEntity);
+        System.out.println(readResponseEntity.getBody());
     }
 
     @Test
     void update() {
+        Package aPackage1 = new Package.Builder().copy(aPackage).setPackageID(015).
+                setMembershipID(025).setHoursPerWeek(8).setPrice(350.00).
+                setPackageName("Medium Package").build();
+        String url = baseURL+"/update";
+        System.out.println(url);
+        System.out.println("updated");
+        ResponseEntity<Package> response = restTemplate.postForEntity(url,aPackage1, Package.class);
+        assertNotNull(response.getBody());
     }
 
     @Test
     void delete() {
-    }
+        String url = baseURL + "delete/" + aPackage.getPackageID();
+        restTemplate.delete(url);
+
+        }
 
     @Test
     void getAll() {
+        String url = baseURL + "all/";
+        System.out.println(url);
+
+        org.springframework.http.HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity =  new HttpEntity<>(null,headers);
+        ResponseEntity<String> getAllResponseEntity = restTemplate.exchange(url, HttpMethod.GET,entity, String.class);
+
+        System.out.println(getAllResponseEntity);
+        System.out.println(getAllResponseEntity.getBody());
+
     }
 }
--- src/test/java/za/ac/cput/controller/ReportControllerTest.java
+++ src/test/java/za/ac/cput/controller/ReportControllerTest.java
@@ -3,7 +3,12 @@ package za.ac.cput.controller;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import za.ac.cput.entity.Report;
+import za.ac.cput.factory.ReportFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,24 +16,71 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 
 class ReportControllerTest {
+    private static Report report = ReportFactory.createReport(022,
+                0021,
+                "2021/03/16",
+                "Weekly");
 
-    @Test
-    void create() {
-    }
+        @Autowired
+        private TestRestTemplate restTemplate;
+        private static final String baseURL = "http://localhost:8080/report";
+
+        @Test
+        void create() {
+            String url = baseURL+"/create";
+            System.out.println(url);
+
+            ResponseEntity<Report> postResponseEntity = restTemplate.postForEntity(url, report,Report.class);
+            assertNotNull(postResponseEntity);
+            assertNotNull(postResponseEntity.getBody());
+            assertEquals(postResponseEntity.getStatusCode(), HttpStatus.OK);
+            report = postResponseEntity.getBody();
+
+            System.out.println(report);
+            assertEquals(report.getReportID(),postResponseEntity.getBody().getReportID());
+        }
 
     @Test
     void read() {
+        String url = baseURL + "read/" + report.getReportID();
+        System.out.println(url);
+
+        ResponseEntity<Report> readResponseEntity = restTemplate.getForEntity(url, Report.class);
+        assertEquals(report.getReportID(), readResponseEntity.getBody().getReportID());
+
+        System.out.println(readResponseEntity);
+        System.out.println(readResponseEntity.getBody());
     }
 
     @Test
     void update() {
+        Report report1 = new Report.Builder().copy(report).setReportID(045).
+                setTrainerID(0015).setReportDate
+                ("2021/08/26").setAttendance
+                ("Daily").build();
+        String url = baseURL+"/update";
+        System.out.println(url);
+        System.out.println("updated");
+        ResponseEntity<Report> response = restTemplate.postForEntity(url,report1, Report.class);
+        assertNotNull(response.getBody());
     }
 
     @Test
     void delete() {
+        String url = baseURL + "delete/" + report.getReportID();
+        restTemplate.delete(url);
     }
 
     @Test
     void getAll() {
+        String url = baseURL + "all/";
+        System.out.println(url);
+
+        org.springframework.http.HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity =  new HttpEntity<>(null,headers);
+        ResponseEntity<String> getAllResponseEntity = restTemplate.exchange(url, HttpMethod.GET,entity, String.class);
+
+        System.out.println(getAllResponseEntity);
+        System.out.println(getAllResponseEntity.getBody());
     }
 }
