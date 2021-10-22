74118356a64cfeee7d8cb62ceb12e0d9622a4d1f
FlamboD <FlamboD2000@gmail.com>
2021-10-22 10:57:23 +0200

    Merged to main

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -3,7 +3,7 @@ package za.ac.cput.service;
 public interface IService <T, ID> {
 
     T create(T t);
-     T read(ID id);
+    T read(ID id);
     T update(T t);
-     boolean delete(ID id);
+    boolean delete(ID id);
 }
